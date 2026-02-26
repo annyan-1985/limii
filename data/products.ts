f2dc1eceb33c4443033979384cc878bb1371d698
ann yan <ayan@atlassian.com>
2026-02-26 14:10:44 +1100

    change

--- data/products.ts
+++ data/products.ts
@@ -13,7 +13,7 @@ export const PRODUCTS: Product[] = [
     id: "p1",
     title: "Custom Uniform Sticker - Primary",
     slug: "uniform-sticker-primary",
-    priceCents: 1699,
+    priceCents: 10,
     imageUrl:"images/pic1.png",
   },
   {
